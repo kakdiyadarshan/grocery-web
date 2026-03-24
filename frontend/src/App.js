0ddd504cce4846f44d3ce7d2281ec9fcd2c75370
VaibhavD17 <vaibhav9723142179@gmail.com>
2026-03-24 12:47:29 +0530

    vaibhav24-03

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,11 @@ import { Toaster } from 'sonner';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Adminroutes from './admin/Adminroutes';
-
+import ContactUs from './pages/ContactUs';
+import FAQs from './pages/FAQs';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import About from './pages/About';
 
 function App() {
   const dispatch = useDispatch();
@@ -15,6 +19,11 @@ function App() {
       <Toaster richColors position="top-center" />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faqs" element={<FAQs />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/shipping-policy" element={<ShippingPolicy />} />
         <Route path="/admin/*" element={<Adminroutes />} />
       </Routes>
     </>
