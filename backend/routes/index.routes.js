761eebe649c57b77e252616d43c24e544e2fe117
parthVaghasiyakal-072 <parthvaghasiya.kalathiyainfo@gmail.com>
2026-03-24 12:01:37 +0530

    Parth24-03

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const indexRoutes = express.Router();
 const upload = require('../helper/imageUpload');
 const { createUser, verifyOtp, resendOtp, userLogin, refreshAccessToken, forgotPassword, forgotVerifyOtp, resetPassword, logout } = require('../controllers/auth.controller');
+const { createCategory, getAllCategories, getCategoryById, updateCategory, deleteCategory } = require('../controllers/category.controller');
+const { auth, authorizeRoles } = require('../middleware/auth.middleware');
 
 // Auth routes
 indexRoutes.post('/register', createUser);
@@ -13,5 +15,12 @@ indexRoutes.post('/forgot-password', forgotPassword);
 indexRoutes.post('/forgotverifyotp', forgotVerifyOtp);
 indexRoutes.post('/reset-password', resetPassword);
 indexRoutes.get('/logout', logout);
+
+// Category routes
+indexRoutes.post('/createCategory', upload.single('categoryImage'), createCategory);
+indexRoutes.get('/getAllCategories', getAllCategories);
+indexRoutes.get('/getCategoryById/:id', getCategoryById);
+indexRoutes.put('/updateCategory/:id', upload.single('categoryImage'), updateCategory);
+indexRoutes.delete('/deleteCategory/:id', deleteCategory);
 
 module.exports = indexRoutes;
